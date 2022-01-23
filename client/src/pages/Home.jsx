7470d939f5146eecc3ddbe4c5f44ab6a214fd2b6
Marie Keita <mashakeita12@gmail.com>
2022-01-23 11:47:28 -0800

    Create and style hero section

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Hero from "../components/Hero/Hero";
+import "../styles/App.scss";
+
+function Home() {
+  return (
+    <>
+      <Hero />
+    </>
+  );
+}
+
+export default Home;
